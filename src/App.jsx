a4ae935afbd65c34a05576a3c0770d996bd4f2d4
camipsv <cami.psv@gmail.com>
2025-04-03 18:14:33 -0300

    Subo hito 2 React

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import Footer from "./components/Footer";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 
 const App = () => {
     return (
     <>
     <Navbar />
-    <Home />
+    <Register/>
+    <Login/>
+    {/* <Home /> */}
     <Footer />
     </>
     )
